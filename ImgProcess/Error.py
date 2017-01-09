7874e470a7752358d500b93b8d7ab4475b85b1e5
Alexander Mueller <alzandermuller@gmail.com>
2017-01-09 16:13:24 -0500

    Fix Indentation

--- ImgProcess/Error.py
+++ ImgProcess/Error.py
@@ -13,7 +13,7 @@ def handle(error):
         debugCmds = Constants.DEBUG_COMMANDS 
 
         if issue == 'Mask':
-            notice  = 'A mask file must have exactly 3 rows of 3 terms, such as the following valid mask:\n\n -1/3 100 1\n  100 0.1 1\n -1.4 123 1'
+            notice = 'A mask file must have exactly 3 rows of 3 terms, such as the following valid mask:\n\n -1/3 100 1\n  100 0.1 1\n -1.4 123 1'
         elif issue == 'Input':
             notice      = 'The following is a valid call: ./ImgProcess.py whale_source.png convolute blur\n%sThe commands and arguments available are as follows:' % (' ' * indents)
             commands    = debugCmds.keys()
